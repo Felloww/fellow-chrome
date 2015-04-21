17c8335aae8e1194b0090c3445766d76a87710e2
Panashe Mahachi <pkmahach@uwaterloo.ca>
2015-04-21 03:10:53 -0400

    Long awaited push

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 $(function() {
 
-  var url = "http://callmenick.com/post/various-css-input-text-styles";
-
   $('#new_artifact').submit(function(e) {
-
    e.preventDefault();
       /*$.post('https://serene-retreat-3166.herokuapp.com/artifacts.json', {"artifact": { "link": "http://www.businessinsider.com/hillary-clinton-will-announce-her-campaign-this-weekend-2015-4", "user_id": 2}});
        */
 
-      var artifacts_url = "https://serene-retreat-3166.herokuapp.com/artifacts.json"
+    var url = "http://www.businessinsider.com/hillary-clinton-will-announce-her-campaign-this-weekend-2015-4";
+
+    var givenTags = $('.theform').find('input[name="tag_list"]').val();
+
+      var artifacts_url = "http://joinfellow.com/artifacts.json";
       $.ajax({
         type: "POST",
         url: artifacts_url,
         data: {"artifact": {
          "link": url,
          "user_id": 2,
+         "tag_list": givenTags
         }},
          success: function(resp){
           console.log(resp);
@@ -25,5 +27,7 @@ $(function() {
       });
 
     this.reset();
+    $('#fellow-button').css('background-color', '#1ABC9C');
+    $('#fellow-button').css('border-color', '#1ABC9C');
     });
 });
